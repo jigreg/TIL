ea8c962fab11796a100310abbe1e0fae54781c19
Yu Seo Jun <jigreg@naver.com>
2021-12-10 22:02:51 +0900

    algo add

--- BOJ STUDY/파이썬 알고리즘 인터뷰/7장 배열/빗물 트래핑.py
+++ BOJ STUDY/파이썬 알고리즘 인터뷰/7장 배열/빗물 트래핑.py
@@ -1,3 +1,19 @@
 # 높이를 입력 받아 비 온후 얼마나 많은 물이 쌓일 수 있는지 계산
 
-###
+def trap(self, height) :
+    if not height:
+        return 0
+
+    volume = 0
+    left, right = 0 , len(height) -1
+    left_max, right_max = height[left],height[right]
+
+    while left < right :
+        left_max, right_max = max(height[left], left_max), max(height[right],right_max)
+        if left_max <= right_max:
+            volume += left_max - height[left]
+            left +=1
+        else :
+            volume += right_max - height[right]
+            right ==1
+        return volume
