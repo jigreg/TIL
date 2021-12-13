4bf734ddd73074e84337092b18d742ec59b7b8da
Yu Seo Jun <jigreg@naver.com>
2021-12-13 21:20:09 +0900

    algo add

--- BOJ STUDY/파이썬 알고리즘 인터뷰/7장 배열/숫자 알고리즘.py
+++ BOJ STUDY/파이썬 알고리즘 인터뷰/7장 배열/숫자 알고리즘.py
@@ -3,3 +3,12 @@ num_list=[]
 for i in range(100):
     num_list.append(i)
 
+index = 0
+for i in num_list:
+    temp = '{:0<3d}'.format(i)
+    num_list[index] = temp
+    index +=1
+
+num_list.sort()
+
+temp = list(map(int,i))
