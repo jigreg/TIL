2c39341cf3cfabbc872b5893c651952aab5b301e
Yu Seo Jun <jigreg@naver.com>
2021-12-16 21:53:50 +0900

    algo add

---
+++ BOJ STUDY/AC/boj3085.py
@@ -0,0 +1,34 @@
+N = int(input())
+candies= [list(input()) for _ in range(N)]
+ans = 1
+def search():
+    global ans 
+    for i in range(N):
+        cnt = 1
+        for j in range(1,N):
+            if candies[i][j-1] == candies[i][j]:
+                cnt +=1
+                ans = max(ans,cnt)
+            else : 
+                cnt = 1
+    for j in range(N):
+        cnt = 1
+        for i in range(1,N):
+            if candies[i][j-1] == candies[i][j]:
+                cnt +=1
+                ans = max(ans,cnt)
+            else : 
+                cnt = 1
+
+
+for i in range(N):
+    for j in range(N):
+        if j+1 < N:
+            candies[i][j], candies[i][j+1] = candies[i][j+1] , candies[i][j]
+            search()
+            candies[i][j], candies[i][j+1] = candies[i][j], candies[i][j+1]
+        if i + 1 < N :
+            candies[i][j], candies[i+1][j] = candies[i+1][j] , candies[i][j]
+            search()
+            candies[i][j], candies[i+1][j] = candies[i][j], candies[i+1][j]
+print(ans)
--- BOJ STUDY/Bronze2/boj2309.py
+++ BOJ STUDY/Bronze2/boj2309.py
@@ -1,3 +1,4 @@
+'''
 hobbit = []
 
 for _ in range(9):
@@ -15,3 +16,13 @@ hobbit.remove(m)
 hobbit.sort()
 for i in hobbit:
     print(i)
+'''
+
+from itertools import combinations
+
+heights = [int(input()) for _ in range(9)]
+for combi in combinations(heights,7):
+    if sum(combi) == 100:
+        for i in sorted(combi):
+            print(i)
+        break
