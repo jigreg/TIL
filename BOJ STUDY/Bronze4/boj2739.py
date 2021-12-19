1e3fae06a2eac15dfa7668dc7a1eeb077e489667
Yu Seo Jun <jigreg@naver.com>
2021-12-19 19:57:13 +0900

    boj add

---
+++ BOJ STUDY/Bronze4/boj2739.py
@@ -0,0 +1,3 @@
+N = int(input())
+for i in range(1,10):
+    print('{0} * {1} = {2}'.format(N,i,N*i))
---
+++ BOJ STUDY/Bronze4/boj8393.py
@@ -0,0 +1,7 @@
+n = int(input())
+N = 0
+for _ in range(n):
+    N += n
+    n -= 1
+
+print(N)
